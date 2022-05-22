dbafde4e9f76bcd48b8053be335048615c2efb79
Vladimir Kreshchenko <vovan.kreshchenko@mail.ru>
2022-05-22 14:58:13 +0300

    Закончена работа с добавлением списка в работу программы(но неправильно)
    Сначала, как и обычно запрашиваются имена файлом, а потом идет цикл
    while, который считывает и записывает данные в список(возможно именно
    тут ошибка, о которой позже), в конце каждого "захода" цикла создается
    новый элемент списка с помощью функции listAppend, далее мы закрываем
    после цикла вводный файл, и возвращаемся в начало списка. Затем идет
    следующий цикл while, где рассчитываются результат в переменные res и
    resv, и таким образом через функцию nextElement проходимся по всем
    действиям. Далее вновь возвращаемся в начало списка, открываем файл для
    записи и через еще один цикл while записываем все в правильной
    форме(возможно ошибка здесь при удалении элемента), далее освобождаем
    память переменных и освобождаем указатель actions, и выводим запрос на
    повторное выполнение

--- Calculator/src/Prop.c
+++ Calculator/src/Prop.c
@@ -19,43 +19,73 @@
                          Язык интерфейса: Русский
  ============================================================================
  */
-typedef struct listAction
-{
-	char operation, mode;
-	double *av, *bv, resv;
-	int size;
-	double a, b, res;
-	struct listAction *nextAction;
-} iAction;
-typedef struct list
-{
-	iAction *head;
-	iAction *current;
-} list;
-iAction *nextAction(list *curr)
-{
-	iAction *next = curr->current->nextAction;
-	curr->current =  next;
-	return next;
-}
-int listAppend(list)
-{
-
-}
 #include <stdio.h>
 #include <stdlib.h>
-//reply - глобальная переменная, используемая циклом while после функции
+typedef struct listElement
+{
+	char operation, mode;
+	double *av, *bv, *resv;
+	int size;
+	double a, b, res;
+	struct listElement *nextElement;
+} iElement;
+typedef struct list
+{
+	iElement *head;
+	iElement *current;
+} list;
+iElement *nextElement(list *inList)
+{
+	iElement *next = inList->current->nextElement;
+	inList->current =  next;
+	return next;
+}
+int listAppend(list *inList, iElement *newElement)
+{
+	if(inList->head == NULL)
+	{
+		inList->head = newElement;
+		inList->current = newElement;
+		return 0;
+	}
+	inList->current->nextElement = newElement;
+	inList->current = newElement;
+	return 0;
+}
+int goHeadElement(list *inList)
+{
+	inList->current =  inList->head;
+	return 0;
+}
+int deleteElement(list *inList)
+{
+	iElement *delete = inList->current;
+	if(delete == inList->head)
+	{
+		inList->head = delete->nextElement;
+		nextElement(inList);
+	}
+	else
+	{
+		goHeadElement(inList);
+		while(inList->current->nextElement != delete) nextElement(inList);
+		inList->current->nextElement = delete->nextElement;
+		if(delete->nextElement != NULL) inList->current = delete->nextElement;
+	}
+	free(delete);
+	return 0;
+}
 int main(int argc,char *argv[])
 //Функция калькулятора
 {
 	char reply;
+	//reply - глобальная переменная, используемая циклом while после функции
 	do
 	{
 		setvbuf(stdout, NULL, _IONBF, 0);
 		setvbuf(stderr, NULL, _IONBF, 0);
 		printf("Здравствуйте, я Калькулятор (^-^)\n");
 		printf("Прежде чем приступить к работе, советую прочитать README с инструкцией по использованию\n");
-		FILE *input, *output;
 		char input_name[259], output_name[259];
 		//Переменные для записи имени файлов
 		printf("Введите название файла формата '.txt', из которого я буду брать данные:");
@@ -63,158 +93,221 @@ int main(int argc,char *argv[])
 		printf("Введите название файла формата '.txt', в который я запишу результаты:");
 		scanf("%s", output_name);
 		//Ввод имени файлов для чтения и записи
-		input = fopen (input_name,"r");
-		output = fopen (output_name,"w");
+		FILE *input = fopen (input_name,"r");
 		//Открытие файлов для чтение и записи
+		list *actions = calloc(1, sizeof(list));
 		char operation, mode;
-		//operation - символ операции, mode - режим работы программы
 		while(fscanf(input, " %c %c", &operation, &mode) != EOF)
 	    //Этот while считывает строчки с данными(то есть несколько действий) до тех пор пока они не закончатся
 		{
+			iElement *newAction = calloc(1, sizeof(iElement));
+			newAction->operation = operation;
+			newAction->mode = mode;
 			if(mode == 'v')
 			//Проверяем символ введенный пользователем для выбора режима
 			{
-				int size;
 				//Эта переменная отвечает за размеры выделяемой памяти для векторов и соответсвенно за размеры векторов
-				double *av, *bv, resv;
 				//Переменные, которым будем присваивать значения векторов
-				fscanf(input, "%i", &size);
-				av = malloc(size*sizeof(double));
-				bv = malloc(size*sizeof(double));
+				fscanf(input, "%i", &newAction->size);
+				newAction->av = malloc(newAction->size*sizeof(double));
+				newAction->bv = malloc(newAction->size*sizeof(double));
 				//Выделение памяти под координаты векторов
-				for(int i = 0; i<size; i++)
-				{
-					fscanf(input, "%lf", &av[i]);
-				}
-				for(int i = 0; i<size; i++)
-				{
-					fscanf(input, "%lf", &bv[i]);
-				}
+				for(int i = 0; i<newAction->size; i++) fscanf(input, "%lf", &newAction->av[i]);
+				for(int i = 0; i<newAction->size; i++) fscanf(input, "%lf", &newAction->bv[i]);
 				//Циклы, отвечающие за ввод коодинат векторов
+			}
+			else if (mode == 's')
+			{
+				fscanf(input, "%lf", &newAction->a);
+				fscanf(input, "%lf", &newAction->b);
+			}
+			listAppend(actions, newAction);
+		}
+		fclose(input);
+		goHeadElement(actions);
+		while(actions->current != NULL)
+		{
+			iElement *thisAction = actions->current;
+			if(actions->current->mode == 'v')
+			{
+				thisAction->resv = malloc(thisAction->size*sizeof(double));
+				switch(operation)
+				//switch для выполнения действия выбранного пользователем
+				{
+					case '+':
+					//Блок отвечающий за сложение векторов
+						for(int i = 0; i<thisAction->size; i++) thisAction->resv[i] = thisAction->av[i]+thisAction->bv[i];
+						//Циклы, выполняющие действие и выводящие результат в правильном виде
+						break;
+					case '-':
+					//Блок отвечающий за вычитание векторов
+						for(int i = 0; i<thisAction->size; i++) thisAction->resv[i] = thisAction->av[i]-thisAction->bv[i];
+						//Циклы, выполняющие действие и выводящие результат в правильном виде
+						break;
+					case '*':
+					//Блок отвечающий за скалярное произведение векторов
+						for(int i = 0; i<thisAction->size; i++) thisAction->res += thisAction->av[i]*thisAction->bv[i];
+						//Циклы, выполняющие действие и выводящие результат в правильном виде
+						break;
+					default:
+						break;
+				}
+
+			}
+			else if (actions->current->mode == 's')
+			{
+				switch(operation)
+				//Разбиваем на действия в зависимости от выбора пользователя
+				{
+					case '+':
+					//Этот блок отвечает за выполнение операции сложения
+						thisAction->res = thisAction->a+thisAction->b;
+						//Пользователь вводит вторую переменную, после чего выводится ответ
+						break;
+					case '-':
+					//Этот блок отвечает за выполнение операции вычитания
+						thisAction->res = thisAction->a-thisAction->b;
+						break;
+					case '*':
+					//Этот блок отвечает за выполнение операции умножения
+						thisAction->res = thisAction->a*thisAction->b;
+						break;
+					case '/':
+					//Этот блок отвечает за выполнение операции деления
+						thisAction->res = thisAction->a/thisAction->b;
+						break;
+					case '^':
+					//Этот блок отвечает за выполнение операции возведения в степень
+					//может рассчитать только положительную целую степень
+						if(thisAction->b>0)
+						{
+							thisAction->res = thisAction->a;
+							for(int i = 1;i<thisAction->b;i++) thisAction->res = thisAction->res*thisAction->a;
+							//Этот цикл рассчитывает результат
+							break;
+						}
+						else if(thisAction->b == 0)
+						{
+						//Если степень равна нулю, то ответ равен 1
+							thisAction->res = 1;
+							break;
+						}
+						else
+						{
+							break;
+						}
+					case '!':
+					//Этот блок отвечает за выполнение операции факториала
+						if(thisAction->a>0)
+						{
+							thisAction->res = thisAction->a;
+							for(int i = 1;i<thisAction->a;i++) thisAction->res = thisAction->res*i;
+							break;
+						}
+						else
+						{
+							if(thisAction->a==0)
+							{
+								thisAction->res = 1;
+								//В случае ввода факториала числа '0' переменная res
+								//принимает значение константы 1
+								break;
+							}
+							else
+							{
+								if(thisAction->a<0) break;
+							}
+							break;
+						}
+					default:
+						break;
+				}
+			}
+			nextElement(actions);
+		}
+		goHeadElement(actions);
+		FILE *output = fopen (output_name,"w");
+		while(actions->current != NULL)
+		{
+			iElement *thisAction = actions->current;
+			if(actions->current->mode == 'v')
+			{
 				switch(operation)
 				//switch для выполнения действия выбранного пользователем
 				{
 					case '+':
 					//Блок отвечающий за сложение векторов
 						fprintf(output, "(");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", av[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->av[i]);
 						fprintf(output, ") + (");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", bv[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->bv[i]);
 						fprintf(output, ") = (");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", av[i]+bv[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->resv[i]);
 						fprintf(output, ")\n");
 						//Циклы, выполняющие действие и выводящие результат в правильном виде
-						free(av);
-						free(bv);
 						//Освобождение памяти
 						break;
 					case '-':
 					//Блок отвечающий за вычитание векторов
 						fprintf(output, "(");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", av[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->av[i]);
 						fprintf(output, ") - (");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", bv[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->bv[i]);
 						fprintf(output, ") = (");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", av[i]-bv[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->resv[i]);
 						fprintf(output, ")\n");
 						//Циклы, выполняющие действие и выводящие результат в правильном виде
-						free(av);
-						free(bv);
 						//Освобождение памяти
 						break;
 					case '*':
 					//Блок отвечающий за скалярное произведение векторов
-						for(int i = 0; i<size; i++)
-						{
-							resv += av[i]*bv[i];
-						}
 						fprintf(output, "(");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", av[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->av[i]);
 						fprintf(output, ") * (");
-						for(int i = 0; i<size; i++)
-						{
-							fprintf(output, "%lf ", bv[i]);
-						}
+						for(int i = 0; i<thisAction->size; i++) fprintf(output, "%lf ", thisAction->bv[i]);
 						fprintf(output, ") = ");
-						fprintf(output, "%lf\n", resv);
+						fprintf(output, "%lf\n", thisAction->res);
 						//Циклы, выполняющие действие и выводящие результат в правильном виде
-						free(av);
-						free(bv);
 						//Освобождение памяти
 						break;
 					default:
 						fprintf(output, "Неизвестная операция\n");
-						fprintf(output, "Я не знаю такой команды, но вы можете обратиться к моему создателю, чтобы он добавил эту операцию 0-0\n");
+						fprintf(output, "Я не знаю такой команды, но вы можете обратиться к моему создателю, чтобы он ввел эту операцию 0-0\n");
 						fprintf(output, "Вот его почта: 'vovan.kreshchenko@mail.ru'\n");
-						//В случае ввода неизвестной операции предлагается
-						//написать разработчику
 						break;
 				}
+				free(thisAction->av);
+				free(thisAction->bv);
+				free(thisAction->resv);
 			}
-			else if (mode == 's')
+			else if (actions->current->mode == 's')
 			{
-				double a, b, res;
-				fscanf(input, "%lf %lf ", &a, &b);
 				switch(operation)
 				//Разбиваем на действия в зависимости от выбора пользователя
 				{
 					case '+':
 					//Этот блок отвечает за выполнение операции сложения
-						fprintf(output, "%lf + %lf = %lf\n",a,b, a+b);
+						fprintf(output, "%lf + %lf = %lf\n",thisAction->a, thisAction->b, thisAction->res);
 						//Пользователь вводит вторую переменную, после чего выводится ответ
 						break;
 					case '-':
 					//Этот блок отвечает за выполнение операции вычитания
-						fprintf(output, "%lf - %lf = %lf\n",a,b, a-b);
+						fprintf(output, "%lf - %lf = %lf\n",thisAction->a, thisAction->b, thisAction->res);
 						break;
 					case '*':
 					//Этот блок отвечает за выполнение операции умножения
-						fprintf(output, "%lf * %lf = %lf\n",a,b, a*b);
+						fprintf(output, "%lf * %lf = %lf\n",thisAction->a, thisAction->b, thisAction->res);
 						break;
 					case '/':
 					//Этот блок отвечает за выполнение операции деления
-						fprintf(output, "%lf / %lf = %lf\n",a,b,a/b);
+						fprintf(output, "%lf / %lf = %lf\n",thisAction->a, thisAction->b, thisAction->res);
 						break;
 					case '^':
 					//Этот блок отвечает за выполнение операции возведения в степень
 					//может рассчитать только положительную целую степень
-						if(b>0)
+						if(thisAction->b>=0)
 						{
-							res = a;
-							for(int i = 1;i<b;i++)
-							//Этот цикл рассчитывает результат
-							{
-								res = res*a;
-							}
-							fprintf(output, "%lf^%.0lf = %lf\n",a,b,res);
-							break;
-						}
-						else if(b == 0)
-						{
-						//Если степень равна нулю, то ответ равен 1
-							res = 1;
-							fprintf(output, "%lf^%.0lf = %lf\n",a,b,res);
+							fprintf(output, "%lf^%.0lf = %lf\n",thisAction->a,thisAction->b,thisAction->res);
 							break;
 						}
 						else
@@ -226,37 +319,17 @@ int main(int argc,char *argv[])
 						}
 					case '!':
 					//Этот блок отвечает за выполнение операции факториала
-						if(a>0)
+						if(thisAction->a>=0)
 						{
-							res = a;
-							for(int i = 1;i<a;i++)
-							{
-								res = res*i;
-							}
-							fprintf(output, "%.0lf! = %.0lf\n",a,res);
+							fprintf(output, "%.0lf! = %.0lf\n",thisAction->a,thisAction->res);
 							break;
 						}
 						else
 						{
-							if(a==0)
-							{
-								res = 1;
-								fprintf(output, "%.0lf! = %.0lf\n",a,res);
-								//В случае ввода факториала числа '0' переменная res
-								//принимает значение константы 1
-								break;
-							}
-							else
-							{
-								if(a<0)
-									{
-									fprintf(output, "Недопустимое значение\n");
-									fprintf(output, "Факториал не бывает отрицательным, попробуйте ввести значение еще раз :(\n");
-									//При попытке вычислить факториал отрицательного числа пользователь
-									//получает предупреждение и предлагается ввести переменную снова
-									break;
-									}
-							}
+							fprintf(output, "Недопустимое значение\n");
+							fprintf(output, "Факториал не бывает отрицательным, попробуйте ввести значение еще раз :(\n");
+							//При попытке вычислить факториал отрицательного числа пользователь
+							//получает предупреждение и предлагается ввести переменную снова
 							break;
 						}
 					default:
@@ -268,14 +341,13 @@ int main(int argc,char *argv[])
 						break;
 				}
 			}
-			else
-			{
-				fprintf(output, "Неизвестный режим операций, пожалуйста, введите 'v' или 's'\n");
-				//Вывод, если символ типа, с какими данными предстоит работать не соответствует
-			}
+			else fprintf(output, "Неизвестный режим операций, пожалуйста, введите 'v' или 's'\n");
+			//Вывод, если символ типа, с какими данными предстоит работать не соответствует
+			fprintf(output, "\n");
+			deleteElement(actions);
 		}
+		free(actions);
 		fclose(output);
-		fclose(input);
 		printf("Результаты записаны в файл\n");
 		printf("Вычислить что-то еще?0-0\n");
 		printf("(Введите 'y' чтобы продолжить и ввести новые файлы для работы или любой другой символ чтобы закончить)");
